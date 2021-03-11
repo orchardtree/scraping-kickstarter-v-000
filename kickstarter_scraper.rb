480a0377edffc4797a30561cbfe2f3ce4fb8be97
Jesse Wenger <wengerj@gmail.com>
2021-03-11 14:01:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,7 +20,7 @@ def create_project_hash
   # iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {:image link => project.css("div.project-thumbnail a img").attribute("src").value, :description => project.css("p.bbcard_blurb").text, :location => project.css("ul.project-meta a span")[1].text, :percentage => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i}
   end
   
   #return the projects create_project_hash
