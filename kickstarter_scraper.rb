ffad4403616248667af342e383cff7ac52907096
Jesse Wenger <wengerj@gmail.com>
2021-03-11 13:45:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,7 @@
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta a span")[1].text
 
 # require libraries/modules here
 require 'nokogiri'
